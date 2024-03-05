b93bae6499f0971a74a057c7a56a066b8f6d7395
Olexander96 <o.sheiko96@gmail.com>
2024-03-05 20:36:57 +0200

    portal for modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,26 +1,39 @@
+import ReactDom from "react-dom";
 import Button from "../UI/Button";
 import "./Modal.css";
+import React from "react";
 
-const Modal = (props) => {
+const ModalContent = (props) => {
   const closeModalHandler = () => {
     props.closeModal(false);
   };
 
   return (
     <div className="modal">
-      <div className="backdrop" onClick={ closeModalHandler }></div>
+      <div className="backdrop" onClick={closeModalHandler}></div>
       <div className="modal-widow">
         <div className="top">
           <span>Некоректний ввід</span>
         </div>
         <div className="bottom">
           <span>{props.error}</span>
-          <Button type="button" onClick={ closeModalHandler }>
+          <Button type="button" onClick={closeModalHandler}>
             Закрити
           </Button>
         </div>
       </div>
     </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <ModalContent closeModal={props.closeModal} error={props.error} />,
+        document.getElementById("modal-wrapper")
+      )}
+    </React.Fragment>
   );
 };
 
